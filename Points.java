2d0e26705870a431f3680670b82166df3ad8df4e
Fhuris <fhuris774@gmail.com>
2017-03-01 16:14:01 +0100

    Add files via upload

---
+++ Points.java
@@ -0,0 +1,26 @@
+package declinaison;
+
+public class Points {
+	
+	private double lat,lon;
+	
+	
+	  public Points (double lat , double lon) {
+	    this.lat =lat;
+	    this.lon =lon;
+	   	  }
+	  
+	  public double getlat() {
+		    return lat;
+		  }
+
+		  public double getlon() {
+		    return lon;
+		  }
+
+		  public String toString() {
+		    return "(" + lat + ";" + lon + ")";
+		  }
+
+
+}
