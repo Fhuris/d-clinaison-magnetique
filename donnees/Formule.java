962f868cfe484d2fa5adf8afb07c85bad3f9919d
Fhuris <fhuris774@gmail.com>
2017-03-15 11:10:36 +0100

    Add files via upload

--- donnees/Formule.java
+++ donnees/Formule.java
@@ -9,8 +9,8 @@ public class Formule {
 	
 	
 	public static double[] calcul(Point p){
-		final double R=3.986005*Math.pow(10,14);//m^3/s²
-		int r=6371000;//rayon de la Terre
+		final double R=3.986005*Math.pow(10,8);//km^3/s²
+		final double rayon=6371200;
 		double X=0;// coordonnées du champ magnetique
 		double Y=0;
 		double Z=0;
@@ -29,20 +29,20 @@ public class Formule {
 				else
 					y += (Lecture.getGv(n,m)*Math.cos(m*p.getLon())+Lecture.getHv(n,m)*Math.sin(m*p.getLon()))*1/(1-Math.pow(teta, 2))*(n+1)*Legendre.getAssociatedFunction(n-1, m-1).evaluate(teta)*n*teta*Legendre.getAssociatedFunction(n, m).evaluate(teta);
 			}
-//			S+=s*Math.pow(R/r,n+1);	
-			X+=x*Math.pow(R/r,n+1);
-			Y+=y*Math.pow(R/r,n+1);
-			Z+=(n+1)*Math.pow(-2*r*R/Math.pow(r,2), n);
+//			S+=s*Math.pow(R/rayon,n+1);	
+			X+=x*Math.pow(R/rayon,n+1);
+			Y+=y*Math.pow(R/rayon,n+1);
+			Z+=(n+1)*Math.pow(-2*rayon*R/Math.pow(rayon,2), n);
 		}
-		tab[0]=R/r*X;
-		tab[1]=R/(r*Math.sin(teta))*Y;
+		tab[0]=R/rayon*X;
+		tab[1]=R/(rayon*Math.sin(teta))*Y;
 		tab[2]=R*Z;
 		return tab; //potentiel scalaire
 		
 		//Calculs des coordonnees champs magnetique
-		//double X = (1/r)*diff(V, teta);
-		//double Y = (1/(r*Math.sin(teta)))*diff(V, z.getLon()*Math.PI/2);
-		//double Z = diff(V, r);
+		//double X = (1/rayon)*diff(V, teta);
+		//double Y = (1/(rayon*Math.sin(teta)))*diff(V, z.getLon()*Math.PI/2);
+		//double Z = diff(V, rayon);
 	}
 }
 // source :http://geomag.nrcan.gc.ca/mag_fld/magref-fr.php
