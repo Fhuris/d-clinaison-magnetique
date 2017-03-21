7edda0ff7de39144a5ed9c2cb60acf4c69f3d688
Fhuris <fhuris774@gmail.com>
2017-03-21 14:17:36 +0100

    Update Leg.java

--- Calcul_declinaison_magnetique/src/donnees/Leg.java
+++ Calcul_declinaison_magnetique/src/donnees/Leg.java
@@ -8,20 +8,17 @@ public class Leg {
 	
 	
 	public static double norm(int n,int m,double phi){
-		double Pn;
-		
 		if (m==0){
-			Pn = Legendre.getAssociatedFunction(n, m).evaluate(phi);
+			return Legendre.getAssociatedFunction(n, m).evaluate(phi);
 		}else{
-			Pn = Math.sqrt(2*(factorial(n-m)/factorial(n+m)))*Legendre.getAssociatedFunction(n, m).evaluate(phi);	
+			return Math.sqrt(2*(factorial(n-m)/factorial(n+m)))*Legendre.getAssociatedFunction(n, m).evaluate(phi);	
 		}
-		return Pn;
 	}
 
 
 	private static int factorial(int i) {
 		int fact = 1; 
-		for (int n = 1;n<i+1; n++) {
+		for (int n = 1;n<=i; n++) {
 			fact *= n;
 		}
 		return fact;
